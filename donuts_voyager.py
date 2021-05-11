bc81a3e6e93ba61b66217a8425cbbadd334bb830
James McCormac <jmccormac001@gmail.com>
2021-05-11 14:00:30 +0100

    adds some new command queue logic

--- donuts_voyager.py
+++ donuts_voyager.py
@@ -6,6 +6,8 @@ import socket as s
 import traceback
 import time
 import threading
+import queue
+import json
 
 # TODO: add logging
 
@@ -24,41 +26,79 @@ class Voyager():
         self.inst = 1
 
         # set up the main polling thread
-        main_thread = threading.Thread(target=self.establish_and_maintain_voyager_connection)
+        main_thread = threading.Thread(target=self.establish_dispatcher)
         main_thread.daemon = True
         main_thread.start()
+
+        self.image_id = 0
+        self.last_poll_time = None
 
         # set up the guiding thread
         #self._guide_condition = threading.Condition()
         #self._guide_latest_frame = None
         # guide_thread = threading.Thread(target=self.__analyse_latest_image)
 
-    def establish_and_maintain_voyager_connection(self):
+        # set up the calibration
+
+    def establish_dispatcher(self):
         """
         Open a connection and maintain it with Voyager
         """
+        # open the socket to Voyager
         self.__open_socket()
 
-        while 1:
-            # keep it alive and listen for jobs
-            polling_str = self.__polling_str()
-            sent = self.__send(polling_str)
-            if sent:
-                print(f"SENT: {polling_str}")
+        # keep it alive and listen for jobs
+        self.__keep_socket_alive()
 
-            # listen for a response
+        # fifo for commands to send to Voyager
+        commands = queue.Queue(maxsize=0)
+
+        # loop until told to stop
+        while 1:
+            # listen for a response or a new job to do
             rec = self.__receive()
             if rec:
                 print(f"RECEIVED: {rec}")
 
-            time.sleep(1)
+                # TODO: dispatch jobs here!
+                # NOTE: donuts calibration:
+                #       1. SEND calibration start
+                #       2. DO calibration procedure: cross pattern with images
+                #       3. Analyse the dat, update the config for direction/magnitude
+                #       4. SEND calibration end (or calibration error if failure)
+                # NOTE: donuts abort
+                #       1. If recieved, stop all threads and end this code
+                # NOTE: donuts recenter <path>
+                #       1. SEND recenter star
+                #       2. Do recentering calculation (including PID loop)
+                #       3. Prepare a response for Voyager
+                #       4. SEND recenter done (or recenter error if failure)
+                # TODO: add communications with Voyager to commands queue
+
+            # do we need to poll again?
+            now = time.time()
+            time_since_last_poll = now - self.last_poll_time
+            if time_since_last_poll > 5 and commands.empty():
+                # ping the keep alive
+                self.__keep_socket_alive()
+
+            # here we pass on any communication to Voyager
+            elif not commands.empty():
+                while not commands.empty():
+                    next_command = commands.get()
+                    sent = self.__send(next_command)
+                    if sent:
+                        print(f"SENT: {next_command}")
+
+
+            #time.sleep(1)
 
     def __open_socket(self):
         """
         Open a connection to Voyager
         """
         self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-        self.socket.settimeout(2.0)
+        self.socket.settimeout(1.0)
         try:
             self.socket.connect((self.socket_ip, self.socket_port))
         except s.error:
@@ -78,9 +118,11 @@ class Voyager():
         Send a message to Voyager
         """
         try:
+            # send the command
             self.socket.sendall(bytes(message, encoding='utf-8'))
             sent = True
-            self.inst += 1
+            # update the last poll time
+            self.last_poll_time = time.time()
         except:
             print(f"Error sending message {message} to Voyager")
             traceback.print_exc()
@@ -98,6 +140,16 @@ class Voyager():
             print("Socket receive timeout, continuing...")
         return message
 
+    def __keep_socket_alive(self):
+        """
+        Convenience method to keep socket open
+        """
+        polling_str = self.__polling_str()
+        sent = self.__send(polling_str)
+        if sent:
+            print(f"SENT: {polling_str}")
+        self.last_poll_time = time.time()
+
     def __polling_str(self):
         """
         Create a polling string
@@ -110,6 +162,31 @@ class Voyager():
         Create a guiding string
         """
         return ""
+
+    def __image_str(self, exptime):
+        """
+        Create a string for taking an image
+        """
+        image_dict = {"method": "RemoteCameraShot",
+                      "params": {"UID": "eaea5429-f5a9-4012-bc9b-f109e605f5d8",
+                                 "Expo": f"{exptime}",
+                                 "Bin": 1,
+                                 "IsROI": "false",
+                                 "ROITYPE": 0,
+                                 "ROIX": 0,
+                                 "ROIY": 0,
+                                 "ROIDX": 0,
+                                 "ROIDY": 0,
+                                 "FilterIndex": 0,
+                                 "ExpoType": 0,
+                                 "SpeedIndex": 0,
+                                 "ReadoutIndex": 0,
+                                 "IsSaveFile": "false",
+                                 "FitFileName": "",
+                                 "Gain": 1,
+                                 "Offset": 0},
+                      "id": self.image_id}
+        return json.dumps(image_dict)
 
 if __name__ == "__main__":
 
