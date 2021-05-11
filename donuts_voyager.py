56923933797347223d8623aa86575b384231236f
James McCormac <jmccormac001@gmail.com>
2021-05-11 22:24:09 +0100

    adds several additions to voyager comms

--- donuts_voyager.py
+++ donuts_voyager.py
@@ -8,8 +8,18 @@ import time
 import threading
 import queue
 import json
+import uuid
+import numpy as np
 
 # TODO: add logging
+
+# pylint: disable=line-too-long
+
+class DonutsStatus():
+    """
+    Current status of Donuts guiding
+    """
+    CALIBRATING, GUIDING, IDLE, UNKNOWN = np.arange(4)
 
 class Voyager():
     """
@@ -25,33 +35,36 @@ class Voyager():
         self.host = config['host']
         self.inst = 1
 
-        # set up the main polling thread
-        main_thread = threading.Thread(target=self.establish_dispatcher)
-        main_thread.daemon = True
-        main_thread.start()
+        # keep track of current status
+        self._status = DonutsStatus.UNKNOWN
 
         self.image_id = 0
         self.last_poll_time = None
 
         # set up the guiding thread
-        #self._guide_condition = threading.Condition()
-        #self._guide_latest_frame = None
-        # guide_thread = threading.Thread(target=self.__analyse_latest_image)
+        self._latest_guide_frame = None
+        self._guide_condition = threading.Condition()
 
-        # set up the calibration
+        # set up a queue to send back results from guide_loop
+        self._results_queue = queue.Queue(maxsize=1)
 
-    def establish_dispatcher(self):
+    def run(self):
         """
         Open a connection and maintain it with Voyager
         """
+        # spawn the guide calculation thread
+        guide_thread = threading.Thread(target=self.__guide_loop)
+        guide_thread.daemon = True
+        guide_thread.start()
+
         # open the socket to Voyager
         self.__open_socket()
 
         # keep it alive and listen for jobs
         self.__keep_socket_alive()
 
-        # fifo for commands to send to Voyager
-        commands = queue.Queue(maxsize=0)
+        # set guiding statuse to IDLE
+        self._status = DonutsStatus.IDLE
 
         # loop until told to stop
         while 1:
@@ -60,52 +73,121 @@ class Voyager():
             if rec:
                 print(f"RECEIVED: {rec}")
 
-                if rec['Event'] == "Polling":
-                    pass
-                elif rec['Event'] == "DonutsCalibrationRequired":
-                    print("Do donuts calibration")
-                    pass
-                elif rec['Event'] == "DonutsRecenterRequired":
-                    print("Do donuts recentering")
-                    pass
-                elif rec['Event'] == "DonutsAbort":
-                    print("Donuts abort requested, dying peacefully")
-                    sys.exit(0)
-                else:
-                    print('Oh dear, something unforseen has occurred...')
-                    pass
+                # handle events
+                if 'Event' in rec.keys():
 
-                # TODO: dispatch jobs here!
-                # NOTE: donuts calibration:
-                #       1. SEND calibration start
-                #       2. DO calibration procedure: cross pattern with images
-                #       3. Analyse the dat, update the config for direction/magnitude
-                #       4. SEND calibration end (or calibration error if failure)
-                # NOTE: donuts abort
-                #       1. If recieved, stop all threads and end this code
-                # NOTE: donuts recenter <path>
-                #       1. SEND recenter star
-                #       2. Do recentering calculation (including PID loop)
-                #       3. Prepare a response for Voyager
-                #       4. SEND recenter done (or recenter error if failure)
-                # TODO: add communications with Voyager to commands queue
+                    if rec['Event'] == "Polling":
+                        pass
+
+                    elif rec['Event'] == "DonutsCalibrationRequired":
+                        print("Do donuts calibration")
+                        # send a dummy command with a small delay for now
+                        self._status = DonutsStatus.CALIBRATING
+                        self.__send_donuts_message_to_voyager(self, "DonutsCalibrationStart")
+                        time.sleep(5)
+                        self.__send_donuts_message_to_voyager(self, "DonutsCalibrationDone")
+                        self._status = DonutsStatus.IDLE
+
+                    elif rec['Event'] == "DonutsRecenterRequired":
+                        print("Do donuts recentering")
+                        # if guider is IDLE, do stuff, otherwise do nothing
+                        if self._status == DonutsStatus.IDLE:
+                            # set the current mode to guiding
+                            self._status = DonutsStatus.GUIDING
+                            # send a DonutsRecenterStart reply
+                            self.__send_donuts_message_to_voyager(self, "DonutsRecenterStart")
+
+                            # keep a local copy of the image to guide on's path
+                            last_image = rec['FitPathAndName']
+
+                            # set the latest image and notify the guide loop thread to wake up
+                            with self._guide_condition:
+                                self._latest_guide_frame = rec['FitPathAndName']
+                                self._guide_condition.notify()
+
+                            # fetch the results from the queue
+                            direction, duration = self._results_queue.get()
+                            print(f"CORRECTION: {direction['x']}:{duration['x']} {direction['y']}:{duration['y']}")
+
+                            # send a pulseGuide command followed by a done
+                            # or by an error command if something happened
+                            try:
+                                uuid_x = str(uuid.uuid4())
+                                self.__send_voyager_pulse_guide(uuid_x, self.image_id, direction['x'], duration['x'])
+                                uuid_y = str(uuid.uuid4())
+                                self.__send_voyager_pulse_guide(uuid_y, self.image_id, direction['y'], duration['y'])
+                                self.image_id += 1
+
+                                # send a DonutsRecenterDone message
+                                self.__send_donuts_message_to_voyager(self, "DonutsRecenterDone")
+                            except:
+                                # send a recentering error
+                                self.__send_donuts_message_to_voyager(self, "DonutsRecenterError", f"Failed to PulseGuide {last_image}")
+
+                            # set the current mode back to IDLE
+                            self._status = DonutsStatus.IDLE
+
+                        else:
+                            print("WARNING: Donuts is busy, skipping...")
+                            # send Voyager a start and a done command to keep it happy
+                            self.__send_donuts_message_to_voyager(self, "DonutsRecenterStart")
+                            self.__send_donuts_message_to_voyager(self, "DonutsRecenterDone")
+
+                    elif rec['Event'] == "DonutsAbort":
+                        print("Donuts abort requested, dying peacefully")
+                        sys.exit(0)
+
+                    # handle responses to remote actions
+                    elif rec['Event'] == "RemoteActionResult":
+                        print("Handle remote action results for a given UID")
+
+                    # erm, something has gone wrong
+                    else:
+                        print('Oh dear, something unforseen has occurred...')
+
+                # handle basic jsonrpc responses
+                elif 'jsonrpc' in rec.keys():
+                    if rec['result'] != 0:
+                        print(f"ERROR: {rec}")
+                    else:
+                        print(f"OK: {rec}")
 
             # do we need to poll again?
             now = time.time()
             time_since_last_poll = now - self.last_poll_time
-            if time_since_last_poll > 5 and commands.empty():
+            if time_since_last_poll > 5:
                 # ping the keep alive
                 self.__keep_socket_alive()
 
             # here we pass on any communication to Voyager
-            while not commands.empty():
-                next_command = commands.get()
-                sent = self.__send(next_command)
-                if sent:
-                    print(f"SENT: {next_command}")
-
+            #while not self._to_voyager.empty():
+            #    next_command = self._to_voyager.get()
+            #    sent = self.__send(next_command)
+            #    if sent:
+            #        print(f"SENT: {next_command}")
 
             #time.sleep(1)
+
+    def __guide_loop(self):
+        """
+        Analyse incoming images for guiding offsets
+        """
+        while 1:
+            # block until a frame is available for processing
+            with self._guide_condition:
+                while self._latest_guide_frame is None:
+                    self._guide_condition.wait()
+
+                frame_path = self._latest_guide_frame
+                self._latest_guide_frame = None
+                # TODO work out guide correction here
+                # Update PID loop
+
+                # test case
+                print(frame_path)
+                direction = {"x": 0, "y": 2}
+                duration = {"x": 1000, "y": 500}
+                self._results_queue.put((direction, duration))
 
     def __open_socket(self):
         """
@@ -158,24 +240,61 @@ class Voyager():
         """
         Convenience method to keep socket open
         """
-        polling_str = self.__polling_str()
+        now = str(time.time())
+        polling = {"Event": "Polling",
+                   "Timestamp": now,
+                   "Host": self.host,
+                   "Inst": self.inst}
+        #polling_str = f"{{\"Event\":\"Polling\",\"Timestamp\":{now},\"Host\":\"{self.host}\",\"Inst\":{self.inst}}}\r\n"
+        polling_str = json.dumps(polling) + "\r\n"
         sent = self.__send(polling_str)
         if sent:
             print(f"SENT: {polling_str}")
         self.last_poll_time = time.time()
 
-    def __polling_str(self):
+    def __send_donuts_message_to_voyager(self, event, error=None):
         """
-        Create a polling string
+        Acknowledge a command from Voyager
         """
         now = str(time.time())
-        return f"{{\"Event\":\"Polling\",\"Timestamp\":{now},\"Host\":\"{self.host}\",\"Inst\":{self.inst}}}\r\n"
+        message = {"Event": event,
+                   "Timestamp": now,
+                   "Host": self.host,
+                   "Inst": self.inst}
+        if error is not None:
+            message['DonutsError'] = error
 
-    def __guide_str(self):
+        msg_str = json.dumps(message) + "\r\n"
+
+        #if error is None:
+        #    msg = f"{{\"Event\":\"{event}\",\"Timestamp\":{now},\"Host\":\"{self.host}\",\"Inst\":{self.inst}}}\r\n"
+        #else:
+        #    msg = f"{{\"Event\":\"{event}\",\"Timestamp\":{now},\"Host\":\"{self.host}\",\"Inst\":{self.inst},\"DonutsError\":\"{error}\"}}\r\n"
+
+        # send the command
+        sent = self.__send(msg_str)
+        if sent:
+            print(f"SENT: {msg_str}")
+        self.last_poll_time = time.time()
+
+    def __send_voyager_pulse_guide(self, uid, idd, direction, duration):
         """
-        Create a guiding string
+        Issue the final post-PID loop guide corrections in direction/duration
+        format. These values are mapped from X and Y offsets during calibration
         """
-        return ""
+        # generate a unique ID for this transmission
+        message = {'method': 'RemotePulseGuide',
+                   'params': {'UID': uid,
+                              'Direction': direction,
+                              'Duration': duration},
+                   'id': idd}
+        msg_str = json.dumps(message) + "\r\n"
+
+        # send the command
+        sent = self.__send(msg_str)
+        if sent:
+            print(f"SENT: {msg_str}")
+        self.last_poll_time = time.time()
 
     def __image_str(self, exptime):
         """
@@ -209,3 +328,4 @@ if __name__ == "__main__":
               'host': 'DESKTOP-CNTF3JR'}
 
     voyager = Voyager(config)
+    voyager.run()
